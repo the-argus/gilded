253c629d2cc55909e41f7de4f1859730e26c3c3a
Ian McFarlane <ifm1110@rit.edu>
2021-11-06 19:47:12 -0400

    chested plate

--- src/main/java/net/fabricmc/gilded/Main.java
+++ src/main/java/net/fabricmc/gilded/Main.java
@@ -3,6 +3,9 @@ package net.fabricmc.gilded;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.gilded.item.GildedChestplate;
+import net.fabricmc.gilded.materials.GildedDiamond;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.item.ArmorItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.util.registry.Registry;
@@ -14,8 +17,12 @@ public class Main implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LogManager.getLogger("modid");
-	public static final Item GILDED_DIAMOND_CHESTPLATE = new GildedChestplate(new FabricItemSettings().group(ItemGroup.COMBAT));
+	public static final Logger LOGGER = LogManager.getLogger("gilded");
+
+	public static final GildedDiamond GILDED_DIAMOND_ARMOR_MATERIAL = new GildedDiamond();
+	//public static final Item GILDED_DIAMOND_MATERIAL_ITEM = new GildedChestplate(new FabricItemSettings().group(ItemGroup.COMBAT));
+
+	public static final Item GILDED_DIAMOND_CHESTPLATE = new ArmorItem(GILDED_DIAMOND_ARMOR_MATERIAL, EquipmentSlot.CHEST, new Item.Settings().group(ItemGroup.COMBAT));
 
 	@Override
 	public void onInitialize() {
--- src/main/java/net/fabricmc/gilded/materials/GildedDiamond.java
+++ src/main/java/net/fabricmc/gilded/materials/GildedDiamond.java
@@ -1,38 +1,42 @@
 package net.fabricmc.gilded.materials;
 
+import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.item.ArmorMaterial;
 import net.minecraft.recipe.Ingredient;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.sound.SoundEvents;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 
 public class GildedDiamond implements ArmorMaterial {
 	private static final int[] BASE_DURABILITY = new int[] {13, 15, 16, 11};
 	private static final int[] PROTECTION_VALUES = new int[] {3, 6, 8, 3};
     private static final int ENCHANTABILITY = 22;
+	private static final float TOUGHNESS = 2.0F;
  
 	@Override
 	public int getDurability(EquipmentSlot slot) {
-		return BASE_DURABILITY[slot.getEntitySlotId()] * X;
+		return this.BASE_DURABILITY[slot.getEntitySlotId()];
 	}
  
 	@Override
 	public int getProtectionAmount(EquipmentSlot slot) {
-		return PROTECTION_VALUES[slot.getEntitySlotId()];
+		return this.PROTECTION_VALUES[slot.getEntitySlotId()];
 	}
  
 	@Override
 	public int getEnchantability() {
-		return ENCHANTABILITY;
+		return this.ENCHANTABILITY;
 	}
  
 	@Override
 	public SoundEvent getEquipSound() {
-		return ITEM_ARMOR_EQUIP_DIAMOND;
+		return SoundEvents.ITEM_ARMOR_EQUIP_DIAMOND;
 	}
  
 	@Override
 	public Ingredient getRepairIngredient() {
-		return Ingredient.ofItems(RegisterItems.Diamond);
+		return Ingredient.ofItems(Registry.ITEM.get(new Identifier("minecraft", "diamond_chestplate")));
 	}
  
 	@Override
@@ -43,7 +47,7 @@ public class GildedDiamond implements ArmorMaterial {
  
 	@Override
 	public float getToughness() {
-		return 2.0F;
+		return this.TOUGHNESS;
 	}
  
 	@Override
